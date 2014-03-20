734f6fff4d7681b0f546dd578f862590ef508a95
Logan Hasson <logan.hasson@gmail.com>
2014-03-20 15:06:21 -0400

    Add mem_cache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ TestScaleApp::Application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  config.cache_store = :mem_cache_store, "172.31.7.136", "172.31.7.139"
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
