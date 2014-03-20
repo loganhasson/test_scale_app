1da562a65bff715122da1190d9801931ade17691
Logan Hasson <logan.hasson@gmail.com>
2014-03-20 14:58:43 -0400

    Add cache_key_for_tasks helper method

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,2 +1,7 @@
 module TasksHelper
+  def cache_key_for_tasks
+    count          = Task.count
+    max_updated_at = Task.maximum(:updated_at).try(:utc).try(:to_s, :number)
+    "tasks/all-#{count}-#{max_updated_at}"
+  end
 end
