47be4785a8818e619aa0cd37d7c3fd4cbd999c4d
Logan Hasson <logan.hasson@gmail.com>
2014-03-20 10:45:22 -0400

    Initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TestScaleApp::Application.config.session_store :cookie_store, key: '_test_scale_app_session'
---
+++ config/unicorn.rb
@@ -0,0 +1,10 @@
+working_directory "/var/www/test_scale_app"
+
+pid "/var/www/test_scale_app/pids/unicorn.pid"
+
+stderr_path "/var/www/test_scale_app/log/unicorn.log"
+stdout_path "/var/www/test_scale_app/log/unicorn.log"
+
+worker_processes 2
+
+timeout 30
